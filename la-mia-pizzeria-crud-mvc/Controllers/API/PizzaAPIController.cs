c41e66f3d607238421889017ddd27432da67c439
giovannitamburini <giovatambu@gmail.com>
2023-10-09 15:16:03 +0200

    added the GET of API controller & fixed the name of controller

---
+++ la-mia-pizzeria-crud-mvc/Controllers/API/PizzaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using la_mia_pizzeria_crud_mvc.Database;
+using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> pizzas = db.Pizzas.ToList();
+
+                return Ok(pizzas);
+            }
+        }
+    }
+}
