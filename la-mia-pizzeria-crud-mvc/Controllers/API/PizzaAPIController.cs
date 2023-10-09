f9888934f5fcbeaf89d2d8adfbee11aa89817440
giovannitamburini <giovatambu@gmail.com>
2023-10-09 15:43:47 +0200

    added the action to search the pizzas by string contained in the name

--- la-mia-pizzeria-crud-mvc/Controllers/API/PizzaAPIController.cs
+++ la-mia-pizzeria-crud-mvc/Controllers/API/PizzaAPIController.cs
@@ -2,6 +2,7 @@
 using la_mia_pizzeria_crud_mvc.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 namespace la_mia_pizzeria_crud_mvc.Controllers.API
@@ -18,6 +19,24 @@ namespace la_mia_pizzeria_crud_mvc.Controllers.API
                 List<Pizza> pizzas = db.Pizzas.Include(pizza => pizza.Ingredients).ToList();
 
                 return Ok(pizzas);
+            }
+        }
+
+        // punto interrogativo perchè potrei ricevere un argomento  nullo
+        [HttpGet]
+        public IActionResult SearchPizzaByStringInTheName(string? stringToSearch)
+        {
+            // gestisco il caso in cui la stringa da cercare nel nome sia nulla
+            if(stringToSearch == null)
+            {
+                return BadRequest(new { Message = "Non hai inserito alcuna stringa da ricercare nel database" });
+            }
+
+            using(PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> foundPizzas = db.Pizzas.Where(pizza => pizza.Name.ToLower().Contains(stringToSearch.ToLower())).ToList();
+
+                return Ok(foundPizzas);
             }
         }
     }
--- la-mia-pizzeria-crud-mvc/Models/Ingredient.cs
+++ la-mia-pizzeria-crud-mvc/Models/Ingredient.cs
@@ -1,7 +1,11 @@
-﻿namespace la_mia_pizzeria_crud_mvc.Models
+﻿using System.Text.Json.Serialization;
+
+namespace la_mia_pizzeria_crud_mvc.Models
 {
     public class Ingredient
     {
+        // il comando [JsonIgnore] serve per dire alla serializzazione in Json di ignorare la proprietà specificata (in questo caso l'id)
+        // [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
 
