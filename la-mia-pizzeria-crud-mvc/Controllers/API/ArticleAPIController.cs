0a2f9e5876be24809757f176536e41367300f830
giovannitamburini <giovatambu@gmail.com>
2023-10-09 14:43:24 +0200

    added new controller for API of pizza

---
+++ la-mia-pizzeria-crud-mvc/Controllers/API/ArticleAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArticleAPIController : ControllerBase
+    {
+    }
+}
