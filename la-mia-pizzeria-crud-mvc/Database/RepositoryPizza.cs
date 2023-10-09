029aaeda63a6eaf38f43c1ac0f4d233ffd17d246
giovannitamburini <giovatambu@gmail.com>
2023-10-09 17:49:12 +0200

    created RepositoryPizza class that implements the interface

---
+++ la-mia-pizzeria-crud-mvc/Database/RepositoryPizza.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+
+namespace la_mia_pizzeria_crud_mvc.Database
+{
+    public class RepositoryPizza : IRepositoryPizza
+    {
+        public bool AddPizza(Pizza pizzaToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePizza(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pizza GetPizzaById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pizza> GetPizzaByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pizza> GetPizzas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePizza(int id, Pizza updatedPizza)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
